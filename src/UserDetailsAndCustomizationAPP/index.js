a6bb4ec5bf9742fa91dcb55788e71756f27ec47b
UppariAkshay <uppari.akshay@gmail.com>
2024-11-20 12:00:34 +0530

    Implemented adding new user using POST request

--- src/UserDetailsAndCustomizationAPP/index.js
+++ src/UserDetailsAndCustomizationAPP/index.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 class UserDetailsAndCustomizationAPP extends Component
 {
-    state = {allUser: []}
+    state = {allUser: [], newUser: {}}
 
     componentDidMount()
     {
@@ -26,12 +26,83 @@ class UserDetailsAndCustomizationAPP extends Component
                 <td>{userData.username}</td>
                 <td>{userData.phone}</td>
                 <td>{userData.email}</td>
-                <td>{userData.addrerss}</td>
+                <td>{userData.address.city}</td>
                 <td>{userData.company.name}</td>
                 <td>{userData.website}</td>
                 <td><button className='btn btn-dark'>Edit</button></td>
             </tr>
         )
+    }
+
+    onChangeName = event => {
+        this.setState(prevState => ({
+            newUser: {...prevState.newUser, name: event.target.value}
+        }))
+    }
+
+    onChangeUserName = event => {
+        this.setState(prevState => ({
+            newUser: {...prevState.newUser, username: event.target.value}
+        }))
+    }
+
+    onChangPhone = event => {
+        this.setState(prevState => ({
+            newUser: {...prevState.newUser, phone: event.target.value}
+        }))
+    }
+
+    onChangeEmail = event => {
+        this.setState(prevState => ({
+            newUser: {...prevState.newUser, email: event.target.value}
+        }))
+    }
+
+    onChangeAddress = event => {
+        this.setState(prevState => ({
+            newUser: {...prevState.newUser, address: {city: event.target.value}}
+        }))
+    }
+
+    onChangCompnay = event => {
+        this.setState(prevState => ({
+            newUser: {...prevState.newUser, company: {name: event.target.value}}
+        }))
+    }
+
+    onChangeWebsite = event => {
+        this.setState(prevState => ({
+            newUser: {...prevState.newUser, website: event.target.value}
+        }))
+    }
+
+    addNewUser = event => {
+        event.preventDefault()
+
+        this.addNewUserIntoDatabase() // calling this function to make a POST request to add new user
+    }
+
+    // this function will make a POST request to add a new user
+    addNewUserIntoDatabase = async () => {
+        const {newUser} = this.state
+
+        const addNewUserUrl = 'https://jsonplaceholder.typicode.com/users'
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(newUser),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+              }
+        }
+
+        const response = await fetch(addNewUserUrl, options) // using POST request to add new user into database
+        const responseInJson = await response.json() 
+
+        console.log(responseInJson) // POST request gives new user data
+
+        this.setState(prevState => ({
+            allUser: [...prevState.allUser, responseInJson]
+        })) // updating state with new user
     }
 
     render()
@@ -41,6 +112,20 @@ class UserDetailsAndCustomizationAPP extends Component
         return (
             <div class='mainContainerDIV'>
                 <h1>User Details</h1>
+                
+                {/* this form will help in adding new user by taking inputs from this form */}
+                <form onSubmit={this.addNewUser} class="container-fluid addNewUserFORM">
+                    <input onChange={this.onChangeName} class="form-control" type='text' placeholder='Enter name' />
+                    <input onChange={this.onChangeUserName} class="form-control" type='text' placeholder='Enter user name' />
+                    <input onChange={this.onChangPhone} class="form-control" type='text' placeholder='Enter Phone' />
+                    <input onChange={this.onChangeEmail} class="form-control" type='text' placeholder='Enter Email' />
+                    <input onChange={this.onChangeAddress} class="form-control" type='text' placeholder='Enter Address' />
+                    <input onChange={this.onChangCompnay} class="form-control" type='text' placeholder='Enter Compnay Name' />
+                    <input onChange={this.onChangeWebsite} class="form-control" type='text' placeholder='Enter Wesbite' />
+                    <button type='submit' class='btn btn-success'>ADD</button>
+                </form>
+
+                
                 <table class="table table-striped">
                     <thead>
                         <tr>
